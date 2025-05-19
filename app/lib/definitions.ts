5fb4428555f1e5e053bf5d6a514eb5fe449abda8
nifi0817 <nikita.filippov@netcracker.com>
2025-05-19 13:17:22 +0500

    fix import of Phone

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -86,3 +86,10 @@ export type InvoiceForm = {
   amount: number;
   status: 'pending' | 'paid';
 };
+
+export type Phone = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+};
